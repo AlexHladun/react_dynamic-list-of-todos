398d04e8d8416d3e3fed669e62c31b81c3dad73f
AlexHladun <gefest.gl@gmail.com>
2019-06-12 14:28:38 +0300

    Updates

---
+++ precache-manifest.5fd77b824412fd30ab9cbf7d9264e848.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3761db8a7956d94f7e18ebc091cca029",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "a05ec776eff6f0aa0f6a",
+    "url": "/react_dynamic-list-of-todos/static/css/main.8dd86e85.chunk.css"
+  },
+  {
+    "revision": "22a965e88c80956f5e2b",
+    "url": "/react_dynamic-list-of-todos/static/js/2.a99c8d40.chunk.js"
+  },
+  {
+    "revision": "a05ec776eff6f0aa0f6a",
+    "url": "/react_dynamic-list-of-todos/static/js/main.81b463d1.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
